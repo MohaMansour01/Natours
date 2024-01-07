8c01eff069f78c3ef9befbf4477034fafd186d8b
moha_01 <mm7.mansour@gmail.com>
2024-01-07 10:58:02 -0800

    added 1st update

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -56,6 +56,7 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   res.redirect(req.originalUrl.split("?")[0])
 });
 
+// EXPORTS
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
 exports.getAllBookings = factory.getAll(Booking);
